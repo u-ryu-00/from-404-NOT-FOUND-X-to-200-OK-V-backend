250fb150e7fc3cf9d15373d52b8cb836597393eb
u-ryu-00 <lwh76104@gmail.com>
2023-06-29 14:30:35 +0900

    Shop 페이지 페이지네이션

--- src/main/java/kr/megaptera/F4T2/services/ProductService.java
+++ src/main/java/kr/megaptera/F4T2/services/ProductService.java
@@ -4,21 +4,25 @@ import jakarta.transaction.Transactional;
 import kr.megaptera.F4T2.dtos.UpdateProductDto;
 import kr.megaptera.F4T2.models.Product;
 import kr.megaptera.F4T2.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+    private Pageable pageable;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, Pageable pageable) {
         this.productRepository = productRepository;
+        this.pageable = pageable;
     }
 
-    public List<Product> list() {
-        return productRepository.findAll();
+    public Page<Product> list(int page) {
+        Pageable pageable = PageRequest.of(page - 1, 8);
+        return productRepository.findAll(pageable);
     }
 
     public Long update(Long id, UpdateProductDto updateProductDto) {
@@ -37,5 +41,11 @@ public class ProductService {
 
     public Product detail(Long id) {
         return productRepository.findById(id).get();
+    }
+
+    public int pages() {
+        Page<Product> productPage = productRepository.findAll(pageable);
+
+        return productPage.getTotalPages();
     }
 }
